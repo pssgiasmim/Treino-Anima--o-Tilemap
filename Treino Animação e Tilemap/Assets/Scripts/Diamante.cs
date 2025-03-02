86690075cfa7d82fc1faaa7ab662826bdb07133a
pssgiasmim <iasmimpassing@gmail.com>
2025-03-02 11:24:16 -0300

    Fazer os diamantes spawnarem na cena e serem coletados

--- Treino Animação e Tilemap/Assets/Scripts/Diamante.cs
+++ Treino Animação e Tilemap/Assets/Scripts/Diamante.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class Diamante : MonoBehaviour
 {
-    void OnTriggerEnter2D(collider2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Jogador")) //Verifica se foi o jogador que tocou no diamante.
         {
-            FindObjectType<SpawnarDiamante>().DiamantesColetados(); //Notifica o spawner que coletou um diamante.
+            FindObjectOfType<SpawnarDiamante>().DiamantesColetados(); //Notifica o spawner que coletou um diamante.
             Destroy(gameObject); //Destroi o diamante coletado.
         }
     }
